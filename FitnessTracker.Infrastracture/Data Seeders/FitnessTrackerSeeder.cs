256400549db698800bf65ce57db863c7b542d512
FilipZytka <filip.zytka@gmail.com>
2023-09-20 21:26:10 +0200

    Added Seeder class called FitnessTrackerSeeder in the Infrastracture Project.

---
+++ FitnessTracker.Infrastracture/Data Seeders/FitnessTrackerSeeder.cs
@@ -0,0 +1,37 @@
+﻿using FitnessTracker.Infrastracture.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Infrastracture.Data_Seeders
+{
+    public class FitnessTrackerSeeder
+    {
+        private readonly FitnessTrackerDbContext _dbContext;
+        public FitnessTrackerSeeder(FitnessTrackerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            
+        }
+        public async Task Seed()
+        {
+            if(await _dbContext.Database.CanConnectAsync())
+            {
+                if(!_dbContext.FitnessTrackers.Any())
+                {
+                    var training1 = new Entities.Entities.FitnessTracker()
+                    {
+                     
+
+                    };
+                   
+
+                }
+
+
+            }
+        }
+    }
+}
